19e6beda949f9b30c70db4c4a8dcc70d980f86e6
michaeljacob6789 <michaeljacob6789@gmail.com>
2025-02-15 08:51:52 +0700

    adding essential and fundamental redux

---
+++ Redux/Fundamental/reducer.jsx
@@ -0,0 +1,195 @@
+// src/features/todos/todosSlice.js
+const initialState = [
+  { id: 0, text: 'Learn React', completed: true },
+  { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+  { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
+]
+
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
+export default function todosReducer(state = initialState, action) {
+    switch (action.type) {
+      case 'todos/todoAdded': {
+        // Can return just the new todos array - no extra object around it
+        return [
+          ...state,
+          {
+            id: nextTodoId(state),
+            text: action.payload,
+            completed: false
+          }
+        ]
+      }
+      case 'todos/todoToggled': {
+        return state.map(todo => {
+          if (todo.id !== action.payload) {
+            return todo
+          }
+  
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        })
+      }
+      default:
+        return state
+    }
+  }
+  
+
+//   src/features/filters/filtersSlice.js
+  const initialState = {
+    status: 'All',
+    colors: []
+  }
+  
+  export default function filtersReducer(state = initialState, action) {
+    switch (action.type) {
+      case 'filters/statusFilterChanged': {
+        return {
+          // Again, one less level of nesting to copy
+          ...state,
+          status: action.payload
+        }
+      }
+      default:
+        return state
+    }
+  }
+
+
+// src/reducer.js
+import { combineReducers } from 'redux'
+
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
+
+const rootReducer = combineReducers({
+  // Define a top-level state field named `todos`, handled by `todosReducer`
+  todos: todosReducer,
+  filters: filtersReducer
+})
+
+export default rootReducer
+
+
+// src/store.js
+import { createStore } from 'redux'
+import rootReducer from './reducer'
+
+const store = createStore(rootReducer)
+
+export default store
+
+// Dispatching Actions
+import store from './store'
+
+// Log the initial state
+console.log('Initial state: ', store.getState())
+// {todos: [....], filters: {status, colors}}
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() =>
+  console.log('State after dispatch: ', store.getState())
+)
+
+// Now, dispatch some actions
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
+
+store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+
+store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
+
+store.dispatch({
+  type: 'filters/colorFilterChanged',
+  payload: { color: 'red', changeType: 'added' }
+})
+
+// Stop listening to state updates
+unsubscribe()
+
+// Dispatch one more action to see what happens
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
+
+// Omit existing React rendering logic
+
+
+// Using Middleware
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducer'
+import { print1, print2, print3 } from './exampleAddons/middleware'
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+// Pass enhancer as the second arg, since there's no preloadedState
+const store = createStore(rootReducer, middlewareEnhancer)
+
+export default store
+
+
+
+// Reading State from the Store with useSelector
+import React from 'react'
+import { useSelector } from 'react-redux'
+import TodoListItem from './TodoListItem'
+
+const selectTodos = state => state.todos
+
+const TodoList = () => {
+  const todos = useSelector(selectTodos)
+
+  // since `todos` is an array, we can loop over it
+  const renderedListItems = todos.map(todo => {
+    return <TodoListItem key={todo.id} todo={todo} />
+  })
+
+  return <ul className="todo-list">{renderedListItems}</ul>
+}
+
+export default TodoList
+
+
+// Dispatching Actions with useDispatch
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+const Header = () => {
+  const [text, setText] = useState('')
+  const dispatch = useDispatch()
+
+  const handleChange = e => setText(e.target.value)
+
+  const handleKeyDown = e => {
+    const trimmedText = e.target.value.trim()
+    // If the user pressed the Enter key:
+    if (e.key === 'Enter' && trimmedText) {
+      // Dispatch the "todo added" action with this text
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      // And clear out the text input
+      setText('')
+    }
+  }
+
+  return (
+    <input
+      type="text"
+      placeholder="What needs to be done?"
+      autoFocus={true}
+      value={text}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  )
+}
+
+export default Header
